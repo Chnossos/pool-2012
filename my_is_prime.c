6b8aecdb6dcc1bd55d04fc87e14c3fdaf8d02a55
Chnossos <chnossos@gmail.com>
2015-08-07 17:34:07 +0200

    Better my_is_prime, better tests

--- my_is_prime.c
+++ my_is_prime.c
@@ -11,18 +11,20 @@
 
 int	my_is_prime(int nb);
 
-int	my_is_prime(int nb)
+int			my_is_prime2(int nb)
 {
-  int	divisor;
+  static unsigned int	prime_divisors[] = { 2, 3, 5, 7 };
+  unsigned int		i;
 
   if (nb <= 1)
     return (0);
-  divisor = 2;
-  while (divisor <= 7)
+  i = -1;
+  while (++i < 4)
   {
-    if (divisor != nb && nb % divisor == 0)
+    if (prime_divisors[i] == nb)
+      return (1);
+    if (nb % prime_divisors[i] == 0)
       return (0);
-    ++divisor;
   }
   return (1);
 }
@@ -32,12 +34,20 @@ int	my_is_prime(int nb)
 #include <assert.h>
 #include <stdio.h>
 
-int	main()
+int		main()
 {
-  assert(my_is_prime(-1) == 0);
-  assert(my_is_prime(0) == 0);
-  assert(my_is_prime(1) == 0);
-  assert(my_is_prime(2) == 0);
+  unsigned int	primes[] =
+  {
+    2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41,
+    43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97
+  };
+  unsigned int	i = -1;
+
+  assert(my_is_prime2(-1) == 0);
+  assert(my_is_prime2(0) == 0);
+  assert(my_is_prime2(1) == 0);
+  while (++i < sizeof(primes) / sizeof(primes[0]))
+    assert(my_is_prime2(primes[i]) == 1);
   puts("Everything is OK");
   return (0);
 }
